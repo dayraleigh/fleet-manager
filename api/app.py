48bae2c68373fe02f3a03ab5b5757aac0e59848e
raleighday <dayraleigh@gmail.com>
2025-11-26 11:58:15 -0700

    initial commit. add css and basic flask server.

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, g, make_response, render_template, request
+
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+app.debug = True
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/test")
+def test():
+    return render_template("growing-unit/index.html")
+
+
+@app.route("/warehouse")
+def warehouse():
+    return render_template("warehouse/index.html")
